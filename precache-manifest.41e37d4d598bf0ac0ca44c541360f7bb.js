2d91495b8a3c4328bf2d51e406d795595bd2f7fd
abhishekalekar <abhishekalekar@gmail.com>
2020-04-19 22:49:11 +1200

    Updates

---
+++ precache-manifest.41e37d4d598bf0ac0ca44c541360f7bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53df5e65d225839774628792c9cc9868",
+    "url": "/DemoWebsite/index.html"
+  },
+  {
+    "revision": "6b0e8247f9d2ca8f70d1",
+    "url": "/DemoWebsite/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "676cd0d9e4cd8a5c1c5d",
+    "url": "/DemoWebsite/static/js/2.e3b2ab51.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/DemoWebsite/static/js/2.e3b2ab51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b0e8247f9d2ca8f70d1",
+    "url": "/DemoWebsite/static/js/main.b0ee200e.chunk.js"
+  },
+  {
+    "revision": "3f92d1a2af02cd7ed1e8",
+    "url": "/DemoWebsite/static/js/runtime-main.018186d9.js"
+  },
+  {
+    "revision": "d61546cda03af82f672458cb776c67a1",
+    "url": "/DemoWebsite/static/media/boatImage.d61546cd.jpg"
+  }
+]);
